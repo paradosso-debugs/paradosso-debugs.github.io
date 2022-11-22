8a16ad97066db207388a9620e17aa9c88441bf3e
paradosso-debug <jocelin.arriaga@yahoo.com>
2022-11-21 20:27:44 -0600

    Se termino el CRUD con javascript, y se agrego media query en CSS

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+let selectRow = null;
+let tasks = [];
+
+updatePageAfterRefresh();
+
+
 function onSubmitForm() {
-    let formData = readForm();
-    insertNewTask(formData);
+    if (validate()) {
+        let formData = readForm();
+        if (selectRow == null) {
+            insertNewTask(formData);
+        }
+        else {
+            updateTask(formData);
+        }
+        resetForm();
+    }
+
 }
 function readForm() {
     let formData = {};
@@ -17,10 +32,73 @@ function insertNewTask(formData) {
     cell2 = newRow.insertCell(1);
     cell2.innerHTML = formData.discription;
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = "<a>Edit</a> <a onClick=deleteTask(this)>Delete</a> "
+    cell3.innerHTML = "<a onClick=editForm(this)>Edit</a> <a onClick=deleteTask(this)>Delete</a> "
+    tasks.push(formData);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+
+function resetForm() {
+    document.getElementById("task").value = "";
+    document.getElementById("discription").value = "";
+    selectRow = null;
 }
 
 function deleteTask(a) {
     let row = a.parentElement.parentElement
     document.getElementById("taskList").deleteRow(row.rowIndex);
+    tasks.splice(row.rowIndex - 1, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
+
+function editForm(a) {
+    selectRow = a.parentElement.parentElement;
+    document.getElementById("task").value = selectRow.cells[0].innerHTML;
+    document.getElementById("discription").value = selectRow.cells[1].innerHTML;
+}
+
+function updateTask(formData) {
+    selectRow.cells[0].innerHTML = formData.task;
+    selectRow.cells[1].innerHTML = formData.discription;
+    tasks.splice(selectRow.rowIndex - 1, 1, { task: formData.task, discription: formData.discription });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function validate() {
+    isValid = true;
+    if (document.getElementById("task").value == "") {
+        isValid = false;
+        document.getElementById("labelId").classList.remove("hide");
+
+    }
+    else {
+        isValid = true;
+        if (!document.getElementById("labelId").classList.contains("hide")) {
+            document.getElementById("labelId").classList.add("hide");
+        }
+    }
+    return isValid;
+}
+
+function updatePageAfterRefresh() {
+    if (localStorage.getItem("tasks") == null) {
+        console.log("Local storage is empty")
+    }
+    else {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+        for (let index = 0; index < tasks.length; index++) {
+            let name = tasks[index].task;
+            let discription2 = tasks[index].discription;
+            document.getElementById("tbody").innerHTML +=
+                `<tr>
+            <td>${name}</td>
+            <td>${discription2}</td>
+            <td>${"<a onClick=editForm(this)>Edit</a> <a onClick=deleteTask(this)>Delete</a> "}</td>
+            </tr>
+            `
+        }
+    }
+
+}
+
+
